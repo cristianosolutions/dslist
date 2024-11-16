8c7e271f8e93570a78a6307e05f99d1f68afb230
Cristiano Magno <161599641+cristianosolutions@users.noreply.github.com>
2024-11-15 21:32:44 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -16,26 +16,29 @@ public class Game {
 	@Id	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
-    private String title;
-    
+    private String title;    
     @Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platforms;
-	private String imgUrl;
-	private String shortDescription;
+	private Double score;
+	private String imgUrl;	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 	}
 	
-	public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl, String shortDescription,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription,
 			String longDescription) {		
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score= score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -76,7 +79,16 @@ public class Game {
 	public String getPlatforms() {
 		return platforms;
 	}
+		
 	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
 	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
 	}
